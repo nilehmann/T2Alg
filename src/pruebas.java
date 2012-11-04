c4abf8557438ad8cf16b6d7b0826cad47323f460
Tefa <tefavidal@gmail.com>
2012-11-04 18:34:10 -0300

    Revert "asd"
    This reverts commit e26190fbd24aa517840d79561a93c4c33a7cb154.

--- src/pruebas.java
+++ src/pruebas.java
@@ -3,17 +3,17 @@ import java.io.IOException;
 
 public class pruebas {
 	public static void main(String[] args) throws IOException {
-		int N=16777216;
+		int N=1048576;
 		VanEmdeBoas VEB= new VanEmdeBoas(N);
 		for (int i=0; i<N;i++){
 			VEB.insert(i);			
 		}
 		System.out.println("inserción lista");	
-		int ant=0; int curr=0;
+		int ant=3; int curr=0;
 		for(int i=1; i<250000;i++){
 			//System.out.println(VEB.findNext(4*i));
-			curr=VEB.findNext(16*i);
-			if(curr!= ant+16)
+			curr=VEB.findNext(4*i+3);
+			if(curr!= ant+4)
 				System.out.println("fail");
 			ant=curr;
 	
